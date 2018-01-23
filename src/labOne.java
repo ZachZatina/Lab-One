8edce06ec13d37724d582050f5c3ecce8fcae8fd
Zach Zatina <zachzatina327@yahoo.com>
2018-01-22 20:54:33 -0500

    added validation

--- src/labOne.java
+++ src/labOne.java
@@ -7,8 +7,8 @@ public class labOne {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//variables being named
+
+		// variables being named
 		Scanner scnr = new Scanner(System.in);
 		char cont = 'y';
 		double length;
@@ -17,11 +17,11 @@ public class labOne {
 		double perimeter;
 		double area;
 		double volume;
-		
-		//Set up of loop in order to continue application based on user preference.
+
+		// Set up of loop in order to continue application based on user preference.
 		while (cont == 'y') {
-			
-			//input of data
+
+			// input of data
 			System.out.print("Enter length: ");
 			length = scnr.nextDouble();
 			System.out.print("Enter width: ");
@@ -29,40 +29,43 @@ public class labOne {
 			System.out.print("If you would like volume calculated enter height here, otherwise enter 0: ");
 			height = scnr.nextDouble();
 			
-			//calculations of perimeter and area
-			perimeter = (length * 2) + (width * 2);
-			area = length * width;
-			
-			//output of wanted data
-			System.out.println("Perimeter: " + perimeter);
-			System.out.println("Area: " + area);
-			
-			//if-else statement to find volume based on height if user wants this data.
-			if (height > 0) {
-				volume = length * width * height;
-				System.out.println("Volume: " + volume);
-			}
+			//Validation for inputs
+			if (length <= 0 || width <= 0 || height < 0) {
+				System.out.println("Invalid inputs, try again.");
+			} 
 			else {
-				System.out.print("");
+				// calculations of perimeter and area
+				perimeter = (length * 2) + (width * 2);
+				area = length * width;
+
+				// output of wanted data
+				System.out.println("Perimeter: " + perimeter);
+				System.out.println("Area: " + area);
+
+				// if-else statement to find volume based on height if user wants this data.
+				if (height > 0) {
+					volume = length * width * height;
+					System.out.println("Volume: " + volume);
+				} else {
+					System.out.print("");
+				}
 			}
-			
-			//this is where user decides if they want to continue using application or not
+
+			// this is where user decides if they want to continue using application or not
 			/*
-			 * Q: had an error when I used a string enter initially
-			 * i.e.: String input = scnr.nextLine();
-			 * 		 cont = input.charAt(0);
-			 * why is this the case as opposed to how i did it below
-			 * which returns no errors.
+			 * Q: had an error when I used a string enter initially i.e.: String input =
+			 * scnr.nextLine(); cont = input.charAt(0); why is this the case as opposed to
+			 * how i did it below which returns no errors.
 			 */
 			System.out.print("Continue? (y/n): ");
 			cont = scnr.next().charAt(0);
 			System.out.println("");
-			
-			}
-		
-		System.out.print("Goodbye!");
-		scnr.close();
-		
+
 		}
 
+		System.out.print("Goodbye!");
+		scnr.close();
+
 	}
+
+}
